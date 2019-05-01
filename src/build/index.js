485f7ed0d2248a107f05cb0093edd58c860910ba
Imre Tabur <imre.tabur@eesti.ee>
2019-05-01 23:24:56 +0300

    version-1.0.1

--- src/build/index.js
+++ src/build/index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var uglify = require("uglify-js");
 
 var code = {
-    "servicejs-test.js": fs.readFileSync("./src/frontend/public/js/servedjs-test.js", "utf8")
+    "servedjs-test.js": fs.readFileSync("./src/frontend/public/js/servedjs-test.js", "utf8")
 };
 
 var options = {
@@ -11,7 +11,7 @@ var options = {
     }
 };
 
-fs.writeFileSync("./src/frontend/public/js/servedjs.min.js", uglify.minify(code, options).code, "utf8");
+fs.writeFileSync("./src/frontend/public/js/servedjs-test.min.js", uglify.minify(code, options).code, "utf8");
 
 fs.createReadStream("./src/frontend/public/js/servedjs-test.js").pipe(fs.createWriteStream("./dist/servedjs-test.js"));
 fs.createReadStream("./src/frontend/public/js/servedjs-test.min.js").pipe(fs.createWriteStream("./dist/servedjs-test.min.js"));
